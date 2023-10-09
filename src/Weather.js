e84962a5cbad9a5e49d1024be91aabb39741f3ac
carolcamaraps <carol@carolinacamara.com>
2023-10-09 18:42:00 -0300

    added Math.round to round up temperature result

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Weather(props) {
 
     function handleResponse(response) {
-alert(`The weather in ${response.data.name} is ${response.data.main.temp}℃`)
+alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}℃`)
     }
     let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
